42efc3922f92662e29cdefbfaba5f7b926c15cba
Voloshyna Jelena <voloshynajelena@gmail.com>
2020-07-19 22:50:38 +0300

    extract component

--- view/src/app/components/extractor/extractor.component.ts
+++ view/src/app/components/extractor/extractor.component.ts
@@ -1,27 +1,20 @@
-import { ChangeDetectionStrategy, Component, OnInit, SimpleChanges, OnChanges, forwardRef } from '@angular/core';
-import { FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-extractor',
   templateUrl: './extractor.component.html',
   styleUrls: ['./extractor.component.less'],
-  providers: [
-    {
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => ExtractorComponent),
-    multi: true,
-    },
-    ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExtractorComponent implements OnInit, OnChanges {
+export class ExtractorComponent implements OnInit {
   code = new FormControl('');
-  constructor() { }
+  link: string;
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-
+  getLink() {
+    this.link = this.code.value;
   }
 }
