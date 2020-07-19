9e111b2fb68395807906dacfd24228ed9c6fd1bd
Дмитрий Волошин <d.volosyn@dynatech.lv>
2020-07-19 20:31:29 +0300

    switch to angular

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.use(express.static(path.join(__dirname, '../view/build')));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('www/build/index.html', { root: __dirname })
-})
+app.use(express.static(path.join(__dirname, '../view/dist/view')));
+// app.get('*', (req: Request, res: Response) => {
+//     res.sendFile('www/build/index.html', { root: __dirname })
+// })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
