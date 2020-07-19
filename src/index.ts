5117afdb0d7151941fb3475893d2df905e031981
Дмитрий Волошин <d.volosyn@dynatech.lv>
2020-07-19 16:09:16 +0300

    add FE part

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 
 import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/', function (req: Request, res: Response) {
-  res.send('Code is magic');
-});
+const PORT = process.env.PORT || 3002;
+
+app.use(express.static(path.join(__dirname, '../view/build')));
+app.get('*', (req, res) => {
+    res.sendFile('www/build/index.html', { root: __dirname })
+})
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
 });
-
