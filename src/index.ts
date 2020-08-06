316fcf6f0a1c04a731e3dd74c30160e6077b8a63
D.Voloshyn <woloshindima@gmail.com>
2020-08-06 21:19:49 +0300

    update build

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import bodyParser from 'body-parser';
 import path from "path";
 import { UrlController } from './Api/UrlController'
 import cors from 'cors';
+import compression from 'compression';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // parse application/json
+app.use(compression())
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../view/dist/view')));
